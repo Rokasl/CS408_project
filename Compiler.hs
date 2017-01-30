c2d0637c9d742572c055b4d4e7cb1b4c13fd0716
rokas <rokas.labeikis@gmail.com>
2017-01-30 13:36:21 +0000

    compiler update, need to implement SHOW

--- Compiler.hs
+++ Compiler.hs
@@ -1,15 +1,26 @@
-
+import Control.Applicative
+import Control.Monad (liftM, ap)
+import Debug.Trace
 
 data  Expr  = Val Int 
-            | Add Expr Expr
-
-
-
+            | Expr :+: Expr
+            deriving Show
+            
 
 newtype CodeGen val = MkCodeGen {
             codeGen :: Int -> ([(Int, String)], Int, val)
-        }
-      
+        } 
+
+-- instance (Show a) => Show (CodeGen a) where
+--     show (MkCodeGen )  = s   
+
+instance Functor CodeGen where
+        fmap = liftM
+
+instance Applicative CodeGen where
+        pure  = return
+        (<*>) = ap
+
 instance Monad CodeGen where
         return val = MkCodeGen $ \ next -> ([], next, val)
         ag >>= a2bg = MkCodeGen $ \ next ->
@@ -19,13 +30,13 @@ instance Monad CodeGen where
                         (bc, next, b) -> (ac ++ bc, next, b)
 
 genDef :: String -> CodeGen Int
-genDef code = codeGen $ \ next -> ([next, code],next + 1, next)
+genDef code = MkCodeGen $ \ next -> ([(next, code)],next + 1, next)
 
 
--- compile :: Expr -> CodeGen Int
--- compile e = help "s" e where
---     help s (Val n) = genDef $
---         "function(s){return{stack:"++ s ++ ", tag:\"num\", data:"++ show n ++"}}"
-    -- help s (e1 + e2) = do
-    --     f2 <- compile e2
-    --     help ("{prev:" ++ s ++ ", tag:\"left\", data:"++ show f2 ++"}") e1    
+compile :: Expr -> CodeGen Int
+compile e = help "s" e where
+    help s (Val n)| trace ((show s) ++ " " ++ show n) True = genDef $
+        "function(s){return{stack:"++ s ++ ", tag:\"num\", data:"++ show n ++"}}"
+    help s (e1 :+: e2) = do
+        f2 <- compile e2
+        help ("{prev:" ++ s ++ ", tag:\"left\", data:"++ show f2 ++"}") e1   
