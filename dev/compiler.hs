53dbc49621f67dd63e01c962ef1de7bd32d991bb
rokas <rokas.labeikis@gmail.com>
2017-02-21 13:23:48 +0000

    added report latex boilerplate, new development of frank compiler

--- dev/compiler.hs
+++ dev/compiler.hs
@@ -33,4 +33,41 @@ instance Monad Counter where
                                         in  runCounter (k a) next'
 
 next :: Counter Int
-next = MkCounter (\ i -> (i, i+1))     
+next = MkCounter (\ i -> (i, i+1)) 
+
+patCompile c (PT x) = do
+  i <- next
+  return ([(x, i)], "env[" ++ show i ++ "]=" ++ c ++ "; ")
+
+patCompile c (PV p) = do
+  (e,m) <- vpatCompile (c ++ ".value") p
+  return (e, "if (" ++ c ++ ".tag===\"value\") {" ++ m ++ "} else { throw(...) }; ")
+
+vpatCompile v (VPV x) = do
+  i <- next
+  return ([(x, i)], "env[" ++ show i ++ "]=" ++ v ++ "; ")
+
+-- jstype JSRun = (JSVal[], JSStack) -> JSMode
+-- jstype JSMode = {stack: JSStack, comp: JSComp}     
+
+-- jstype JSComp
+--   = {tag="value", value: JSVal}
+--   | {tag="command", command: Command, args: JSVal[], resume: JSVal}
+
+-- (Command is String)
+
+-- jstype JSVal
+--   = {tag="atom", atom: String}
+--   | {tag="int", int: Int}
+--   | {tag="pair", car: JSValue, cdr: JSValue}
+--   | {tag="fun",
+--       handles: Command[][],
+--       matcher: (JSComp[]) -> JSMatch,
+--       branches: JSRun[]}
+
+-- jstype JSMatch
+--   = null  -- bad news
+--   | {branch: int, env: JSVal[]}
+
+-- if we know the EnvTable for each case in a match, we can compile the
+-- expressions as JSRun things, then collect them in a "branches" array.
