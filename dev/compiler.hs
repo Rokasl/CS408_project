6cce2c736c6a6b6cbc46cb23b53456e9b619bd84
Conor McBride <conor@strictlypositive.org>
2017-02-23 17:23:24 +0000

    Conor gives the next lesson in writing compilers: expressions, program lines, and trying successive program lines

--- dev/compiler.hs
+++ dev/compiler.hs
@@ -37,9 +37,10 @@ next = MkCounter (\ i -> (i, i+1))
 
 type JSExp = String
 type JSStmt = String
+type JSMode = String
 
 matchFail :: JSStmt
-matchFail = "throw( ... );" -- FIXME!
+matchFail = "throw(\"no match\");" -- FIXME!
 
 patCompile  :: JSExp -- representing the scrutinee of the match
             -> Pat   -- the Shonky computation pattern
@@ -89,6 +90,100 @@ listOf comp arr ps = go (zip [0..] ps) where
     return (t1 ++ t2, e1 ++ e2)
 
 
+-- lifted from earlier experiment
+
+newtype CodeGen val = MkCodeGen {
+            codeGen :: Int -- next available number (for naming helper functions)
+                       -> ([(  Int        -- this number...
+                            ,  String     -- ...defined as this JS code
+                           )]
+                          ,  Int       -- next available number after compilation
+                          ,  val       -- result of compilation process
+                          )  -- usually the list of definitions will start with
+        }                    -- the input "next number" and go up to just before
+                             -- the output "next number"
+                             
+
+instance Functor CodeGen where
+        fmap = liftM
+
+instance Applicative CodeGen where
+        pure  = return
+        (<*>) = ap
+
+instance Monad CodeGen where
+        return val = MkCodeGen $ \ next -> ([], next, val)
+        ag >>= a2bg = MkCodeGen $ \ next ->
+            case codeGen ag next of
+                (ac, next, a) -> case 
+                    codeGen(a2bg a) next of
+                        (bc, next, b) -> (ac ++ bc, next, b)
+
+genDef :: String -> CodeGen Int -- make a definition and return its number
+genDef code = MkCodeGen $ \ next -> ([(next, code)],next + 1, next)
+
+
+expFun
+  :: EnvTable     -- maps variable names to env indices
+  -> Exp          -- Shonky source code
+  -> CodeGen Int
+
+expFun xis e = do
+  js <- expCompile xis "stk" e
+  genDef ("function(stk,env){return " ++ js ++ "}")
+
+expCompile
+  :: EnvTable     -- maps variable names to env indices
+  -> JSExp        -- stack
+  -> Exp          -- Shonky source code
+  -> CodeGen JSExp
+
+expCompile xis stk (EV x) = case lookup x xis of
+  Nothing -> error "it's not a pattern variable, but is it a top-level function?"
+  Just i -> return $ "{stack:" ++ stk ++ ", comp:{tag:\"value\", value:env[" ++ show i ++ "]}}"
+
+expCompile xis stk (EA a) = 
+  return $ "{stack:" ++ stk ++ ", comp:{tag:\"value\", value:{tag:\"atom\", atom:\"" ++ a ++ "\"}}}"
+
+expCompile xis stk (ecar :& ecdr) = do
+  fcdr <- expFun xis ecdr
+  expCompile xis
+    ("{prev: " ++ stk ++ ", tag:\"car\", env:env, cdr:" ++ show fcdr ++ "}")
+    ecar
+
+
+lineCompile :: ([Pat], Exp) -> CodeGen JSStmt
+lineCompile (ps, e) = do
+  let ((xis, patJS), _) = runCounter (listOf patCompile "args" ps) 0
+  expJS <- expCompile xis "stk" e
+  return $ "{" ++ patJS ++ "return " ++ expJS ++ "\n}"
+
+-- codeGen (lineCompile ([PV (VPV "x"), PV (VPV "y")], EV "y" :& EV "x")) 0
+
+linesCompile :: [([Pat], Exp)] -> CodeGen JSStmt
+linesCompile [] = return "throw(\"undefined function\")"
+linesCompile (l : ls) = do
+  ctry   <- lineCompile l
+  ccatch <- linesCompile ls
+  return $ "try " ++ ctry ++ " catch (err) {" ++ ccatch ++ "}"
+
+funCompile :: [([Pat], Exp)] -> CodeGen JSStmt
+funCompile ls = do
+  c <- linesCompile ls
+  return $ "function(stk,args){var env=[];\n" ++ c ++ "\n}"
+
+-- codeGen (funCompile [([PV (VPV "x"), PV (VPV "y")], EV "y" :& EV "x")]) 0
+
+
+topLevelCompile :: [Def Exp] -> CodeGen [(Int, JSStmt)]
+topLevelCompile ds = do
+  let fs = [(f, (h, pse)) | DF f h pse <- ds]
+  let ftable = zipWith (\ (f, _) i -> (f, i)) fs [0..]
+  -- now adjust compiler functions to take an ftable (preferably by extending the CodeGen monad)
+  -- now compile all the top level functions and put them in an array
+  error "not done yet"
+
+
 -- jstype JSRun = (JSVal[], JSStack) -> JSMode
 -- jstype JSMode = {stack: JSStack, comp: JSComp}     
 
@@ -102,10 +197,7 @@ listOf comp arr ps = go (zip [0..] ps) where
 --   = {tag="atom", atom: String}
 --   | {tag="int", int: Int}
 --   | {tag="pair", car: JSValue, cdr: JSValue}
---   | {tag="fun",
---       handles: Command[][],
---       matcher: (JSComp[]) -> JSMatch,
---       branches: JSRun[]}
+--   | {tag="toplevelfun", toplevelfun: Int}
 
 -- jstype JSMatch
 --   = null  -- bad news
